871b54bbda1697b5ad30b407e0961c91914abf4f
Muhammad Hanan Rafif Farizta <hananfariztaa@gmail.com>
2022-10-25 23:46:53 +0700

    feat/add home_page

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,11 +1,11 @@
-import 'package:mobile_smart_dashboard/main.dart';
+import 'package:mobile_smart_dashboard/pages/home_page/home_page.dart';
 import 'package:mobile_smart_dashboard/routes/app_page.dart';
 import "package:get/get.dart";
 
 class AppPages {
-  static const initial = Routes.main;
+  static const initial = Routes.homepage;
 
   static final routes = [
-    GetPage(name: Routes.main, page: () => const MobileSmartDashboard()),
+    GetPage(name: Routes.homepage, page: () => const HomePage()),
   ];
 }
