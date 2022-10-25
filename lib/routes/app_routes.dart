6b965b80b00c0a1341d4817017eed9c04a49f052
Muhammad Hanan Rafif Farizta <hananfariztaa@gmail.com>
2022-10-25 22:41:39 +0700

    feat/add data_diri

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,11 +1,12 @@
 import 'package:mobile_smart_dashboard/main.dart';
+import 'package:mobile_smart_dashboard/pages/data_diri/data_diri_page.dart';
 import 'package:mobile_smart_dashboard/routes/app_page.dart';
 import "package:get/get.dart";
 
 class AppPages {
-  static const initial = Routes.main;
+  static const initial = Routes.datadiri;
 
   static final routes = [
-    GetPage(name: Routes.main, page: () => const MobileSmartDashboard()),
+    GetPage(name: Routes.datadiri, page: () => const DataDiriPage()),
   ];
 }
