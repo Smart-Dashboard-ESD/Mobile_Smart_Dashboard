8ec76e7ce232a14e75340aaddd08139da5d9cc59
Muhammad Hanan Rafif Farizta <hananfariztaa@gmail.com>
2022-10-25 23:14:09 +0700

    feature/add alamat_page

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,11 +1,12 @@
 import 'package:mobile_smart_dashboard/main.dart';
+import 'package:mobile_smart_dashboard/pages/alamat/alamat_page.dart';
 import 'package:mobile_smart_dashboard/routes/app_page.dart';
 import "package:get/get.dart";
 
 class AppPages {
-  static const initial = Routes.main;
+  static const initial = Routes.alamat;
 
   static final routes = [
-    GetPage(name: Routes.main, page: () => const MobileSmartDashboard()),
+    GetPage(name: Routes.alamat, page: () => const AlamatPage()),
   ];
 }
