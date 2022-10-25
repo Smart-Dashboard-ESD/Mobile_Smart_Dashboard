8fc9ae4eadbff23109b90a59776fce7cc77be572
Muhammad Hanan Rafif Farizta <hananfariztaa@gmail.com>
2022-10-25 23:42:49 +0700

    feat/add sign_in

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,11 +1,11 @@
-import 'package:mobile_smart_dashboard/main.dart';
+import 'package:mobile_smart_dashboard/pages/sign_in/sign_in_page.dart';
 import 'package:mobile_smart_dashboard/routes/app_page.dart';
 import "package:get/get.dart";
 
 class AppPages {
-  static const initial = Routes.main;
+  static const initial = Routes.signin;
 
   static final routes = [
-    GetPage(name: Routes.main, page: () => const MobileSmartDashboard()),
+    GetPage(name: Routes.signin, page: () => const SignInPage()),
   ];
 }
