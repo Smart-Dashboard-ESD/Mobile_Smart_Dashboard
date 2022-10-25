e27cb1256b6a6e28d3cd8a9796b815cb75168e98
Muhammad Hanan Rafif Farizta <hananfariztaa@gmail.com>
2022-10-25 22:34:21 +0700

    feat/add nomor_penduduk

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,11 +1,12 @@
 import 'package:mobile_smart_dashboard/main.dart';
+import 'package:mobile_smart_dashboard/pages/nomor_penduduk/nomor_penduduk_page.dart';
 import 'package:mobile_smart_dashboard/routes/app_page.dart';
 import "package:get/get.dart";
 
 class AppPages {
-  static const initial = Routes.main;
+  static const initial = Routes.nomorpenduduk;
 
   static final routes = [
-    GetPage(name: Routes.main, page: () => const MobileSmartDashboard()),
+    GetPage(name: Routes.nomorpenduduk, page: () => const NomorPendudukPage()),
   ];
 }
